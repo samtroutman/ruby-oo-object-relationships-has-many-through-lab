6e487ef9bc9d592cf212850a5d1da997b9d6c25f
Sam <sam@Sams-MacBook-Air.local>
2021-01-02 23:37:43 -0500

    commit tree 0cb487d9386add61953795a852b81365af47f1c2

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,17 @@
+class Appointment
 
+    attr_accessor :date, :patient, :doctor
+    @@all = []
+  
+    def initialize(patient, date, doctor)
+        @date = date
+        @patient = patient
+      @doctor = doctor
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+  end 
+  
